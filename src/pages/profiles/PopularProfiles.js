46a0fc0eca5fe810e2c516ba3abc49b6f6f3196d
Mees <rasmees123@gmail.com>
2025-06-19 01:01:56 +0200

    ADD: CSS styling for followed profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -18,6 +18,7 @@ const PopularProfiles = ({ mobile }) => {
       {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
+          <hr>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
