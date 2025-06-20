3e16b4f5f4da19420d4a10109ad49a6cd1566d58
Mees <rasmees123@gmail.com>
2025-06-21 00:59:29 +0200

    ADD: site name next to logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -105,6 +105,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+        <h1>Reeltalk</h1>
         {currentUser && addPostIcon}
         <button
           ref={ref}
